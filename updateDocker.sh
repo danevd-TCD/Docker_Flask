b88b24479856dbeec856442f78b1a08df486e577
Daniel Danev <danevd@tcd.ie>
2021-06-14 13:57:54 +0100

    modified updateDocker; not sure if working properly yet

--- updateDocker.sh
+++ updateDocker.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-#imageName=xx:my-image
-imageName=docker-flask_web
-containerName=my-container
-
-docker build -t $imageName -f Dockerfile  .
-
-echo Delete old container...
-docker rm -f $containerName
-
-echo Run new container...
-docker run -d -p 5000:5000 --name $containerName $imageName
+docker-compose down
+docker-compose up --build -d
