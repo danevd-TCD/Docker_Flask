f14e382402ec46be4f99893ab3d8fdc8816b64e5
Daniel Danev <danevd@tcd.ie>
2021-06-14 12:07:46 +0100

    updated docker-compose with persistent volume

--- flaskFile.py
+++ flaskFile.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/var/Team10_App/frontend')
+
+#@app.route('/')
+#def root():
+#	return("Hello")
 
 @app.route('/')
 def root():
-	return("Hello")
+        return render_template('templates/index.html')
+
 
 if __name__ == "__main__":
 	app.run()
