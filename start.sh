940c970b74edbd9534477abab87dee9bfa0addf9
Daniel (Remote) <danevd@tcd.ie>
2021-06-21 20:03:04 +0000

    updated conf, attempted to get automated certbot working

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/sbin/apache2ctl -D FOREGROUND;certbot --apache --non-interactive --staging --quiet --agree-tos -d csi6220-4-vm1.ucd.ie --no-autorenew -m daniel.danev@ucdconnect.ie --no-eff-email
+/usr/sbin/apache2ctl -D FOREGROUND;certbot --apache --non-interactive --staging --quiet --agree-tos -d danev.xyz -d www.danev.xyz -m danevd@tcd.ie --no-eff-email
